f17ad3edd3951da33d99b3ae28821591bc161a5f
Your Name <you@example.com>
2024-06-19 21:38:00 +0200

    fixes

--- apteka/apteka/settings.py
+++ apteka/apteka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bbrhtz20hc&6nl(sr(x4+82w6b@q=g)!wuvb+dfdmiq%t-339&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
